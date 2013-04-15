28c974111dbb245c77795be76f930c31c281ce28
Mordechai Rynderman <rynderman@gmail.com>
2013-04-15 15:10:56 -0400

    updated motor units

--- mmRoger2013/RogerProjects/project1/MotorUnits.c
+++ mmRoger2013/RogerProjects/project1/MotorUnits.c
@@ -7,7 +7,6 @@
 /*************************************************************************/
 #include <math.h>
 #include <stdio.h>
-// #include "Xkw/Xkw.h"
 
 #include "Roger.h"
 #include "simulate.h"
@@ -23,16 +22,11 @@ Robot * roger;
 double time;
 {
     update_setpoints(roger);
-    
     // turn setpoint references into torques
     PDController_eyes(roger, time);
-    PDController_arms(roger, time);
+    //PDController_arms(roger, time);
     PDController_base(roger,time);
 }
-
-/*************************************************************************/
-/* PROJECT #1 - COMPLETE THE FOLLOWING CONTROL PROCEDURES                */
-/*************************************************************************/
 
 // gains for the PD controllers for eyes
 double kp_eye = 1.0; //KP_EYE
@@ -49,49 +43,8 @@ double time;
     
     for (i = 0; i < NEYES; i++) {
         theta_error = roger->eyes_setpoint[i] - roger->eye_theta[i];
-        
         theta_ddot_des = kp_eye*theta_error - kd_eye*roger->eye_theta_dot[i];
-        
-        // feedforward compensator
-        // roger->eye_torque[i] = (M_EYE*SQR(L_EYE)) * theta_ddot_des +
-        //       M_EYE*GRAVITY*L_EYE*cos(roger->eye_theta[i]);
         roger->eye_torque[i] = theta_ddot_des;
-    }
-}
-
-// gains for the PD controllers for arms
-double kp_arm = 400; //KP_ARM;
-double kd_arm = 17; //KD_ARM;
-
-/* PROJECT #1.2 - PD CONTROLLER FOR THE ARMS                             */
-/* setpoints - joint angles in radians for the shoulders and elbows      */
-PDController_arms(roger, time)
-Robot * roger;
-double time;
-{
-    int i;
-    double theta_error[2], acc[2], M[2][2], V[2], G[2];
-    
-    for (i=LEFT; i<=RIGHT; ++i) {
-        // PDcontrol - desired accelerations
-        theta_error[0] = roger->arm_setpoint[i][0] - roger->arm_theta[i][0];
-        theta_error[1] = roger->arm_setpoint[i][1] - roger->arm_theta[i][1];
-        
-        //shoulder - bound error -pi < error < +pi
-        while (theta_error[0] > M_PI) theta_error[0] -= 2.0 * M_PI;
-        while (theta_error[0] < -M_PI) theta_error[0] += 2.0 * M_PI;
-        //elbow - bound error -pi < error < +pi
-        while (theta_error[1] > M_PI) theta_error[1] -= 2.0 * M_PI;
-        while (theta_error[1] < -M_PI) theta_error[1] += 2.0 * M_PI;
-        
-        // tune kp_arm and kd_arm by changing their value using enter_params()
-        acc[0] = kp_arm * (theta_error[0]) +
-        kd_arm * (0.0 - roger->arm_theta_dot[i][0]);
-        acc[1] = kp_arm * (theta_error[1]) +
-        kd_arm * (0.0 - roger->arm_theta_dot[i][1]);
-        
-        roger->arm_torque[i][0] = acc[0];
-        roger->arm_torque[i][1] = acc[1];
     }
 }
 
@@ -172,15 +125,7 @@ double time;
 /*************************************************************************/
 enter_params()
 {
-    // put anything in here that you would like to change at run-time
-    // without re-compiling user project codes
-    
-    //  printf(" K = %6.4lf   B = %6.4lf\n", kp_eye, kd_eye);
-    //  printf("enter K\n"); fflush(stdout);
-    //  scanf("%lf", &kp_eye);
-    //	kd_eye = sqrt(4.0*kp_eye*I_EYE);
-    
-    printf("ARM: K=%6.4lf  B=%6.4lf\n", kp_arm, kd_arm);
-    printf("ARM: enter 'K B'\n"); fflush(stdout);
-    scanf("%lf %lf", &kp_arm, &kd_arm);
+    //printf("ARM: K=%6.4lf  B=%6.4lf\n", kp_arm, kd_arm);
+    //printf("ARM: enter 'K B'\n"); fflush(stdout);
+    //scanf("%lf %lf", &kp_arm, &kd_arm);
 }
